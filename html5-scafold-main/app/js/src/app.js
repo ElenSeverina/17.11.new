93a91480f95d7b3b25116026b8b632aaadb6d16a
HelenSeverina <helen.severina23@gmail.com>
2022-11-18 01:07:29 +0200

    com7

--- html5-scafold-main/app/js/src/app.js
+++ html5-scafold-main/app/js/src/app.js
@@ -1,3 +1,6 @@
+/*
 (function() {
   (new Example()).hello();
 })();
+*/
+
---
+++ html5-scafold-main/site/js/app.js
@@ -0,0 +1,2 @@
+class Example{hello(){document.getElementById("example").textContent="Hello world! This is HTML5 Boilerplate."}}(new Example).hello();
+//# sourceMappingURL=app.js.map
